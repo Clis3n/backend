f9f09c53d21becd767d8059b99883291b39d1ace
Clis3n <clisenardylaksonowicaksono@mail.ugm.ac.id>
2025-04-19 03:30:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
 });
 
 // Jalankan Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app
   .listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
