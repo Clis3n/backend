3d56b44a5de215e29546ef6bf95995905f3729e6
clis3n <clisenardylaksonowicaksono@mail.ugm.ac.id>
2025-04-19 07:43:36 +0700

    aaa

--- models/User.js
+++ models/User.js
@@ -4,11 +4,11 @@ const UserSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  avatar: { type: String }, // Ini penting, bukan picture
+  avatar: { type: String },
   role: {
     type: String,
-    enum: ['user'],
-    default: 'user', // WAJIB: default 'user'
+    enum: ['user', 'admin'],
+    default: 'user', // Peran default adalah 'user'
   },
 });
 
